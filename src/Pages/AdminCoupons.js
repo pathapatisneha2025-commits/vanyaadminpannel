f577f37a9874b789310e70218a030d05ddc0677f
Sneha-datla <datla.sneha1@gmail.com>
2026-03-18 15:14:35 +0530

    changes made

--- src/Pages/AdminCoupons.js
+++ src/Pages/AdminCoupons.js
@@ -40,7 +40,7 @@ const AddCouponForm = () => {
     e.preventDefault();
 
     try {
-      const res = await fetch("https://vanyabackenddatabase.onrender.com/cart/coupons/all", {
+      const res = await fetch("https://vanyabackenddatabase.onrender.com/cart/add/coupons", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(form),
